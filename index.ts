11cfa61df3b0ea0de5e90f79dbf3beda3e075593
firesquid6 <jdeiss06@gmail.com>
2024-05-13 21:45:41 -0500

    add packages plugin

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import Elysia from "elysia"
 import { cors } from "@elysiajs/cors";
 import { plugin as usersPlugin } from "./users"
+import { plugin as packagesPlugin } from "./packages";
 
 
 const app = new Elysia()
   .use(cors())
+  .use(packagesPlugin)
   .use(usersPlugin)
 
 
