76f12f2dbe920eac899a82c1102126c6d6da7160
firesquid6 <jdeiss06@gmail.com>
2024-05-18 15:23:26 -0500

    move files

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
 import Elysia from "elysia"
 import { cors } from "@elysiajs/cors";
-import { plugin as usersPlugin } from "./users"
-import { plugin as packagesPlugin } from "./packages";
+import { plugin as usersPlugin } from "./routes/users"
+import { plugin as packagesPlugin } from "./routes/packages";
 
 
 const app = new Elysia()
   .use(cors())
   .use(packagesPlugin)
   .use(usersPlugin)
-
-
 
 app.listen(3000)
